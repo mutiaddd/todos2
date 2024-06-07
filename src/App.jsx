4e5c8df583ea06a52e0b1e0436228c48192be089
mutiadonda@gmail.com <mutiadonda@gmail.com>
2024-06-07 22:35:42 +0700

    Develop add todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Todos from './components/Todos'
+import TodoForm from './components/TodoForm'
 
 const todoData =[
 {
@@ -33,10 +34,34 @@ function App() {
     setTodos(updatedTodos)
   }
 
+  const deleteTodo = (id) =>{
+    const update = todos.filter((todo)=>{
+      return todo.id !== id;
+    });
+
+    setTodos(update)
+  };
+
+  const addTodo = (todoTitle) => {
+    if (todoTitle === '') {
+      return
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    }
+
+    const updatedTodos = todos.concat(newTodo)
+    setTodos(updatedTodos)
+  }
+
   return(
   <div style={styles.container}>
   <h1 style={styles.title}>My Todo List</h1>
-  <Todos todos={todos} toggleCompleted={toggleCompleted}  />
+  <TodoForm addTodo={addTodo} />
+  <Todos todos={todos} toggleCompleted={toggleCompleted} deleteTodo={deleteTodo} />
   </div>);
 }
 
